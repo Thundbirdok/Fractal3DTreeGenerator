05772d66f52097bb43cba4a8b55a403f45aba56e
ip613s07 <vyacheslavgorohov@mail.ru>
2020-11-26 11:18:15 +0700

    Add Combined meshes

--- TreeGenerator/Assets/Scripts/TreeGenerator.cs
+++ TreeGenerator/Assets/Scripts/TreeGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -38,54 +39,97 @@ public class TreeGenerator : MonoBehaviour
     private void GenerateTree()
     {
 
-        float radiansBetweenBranches = 360 / branchesNumber /** 2 * Mathf.PI*/;
+        CreatePool(out GameObject branchesPool, branch, "Branches Pool");
+        CreatePool(out GameObject leafsPool, leaf, "Leafs Pool");
+
+        float radiansBetweenBranches = 360 / branchesNumber;
 
         Queue<GameObject> branches = new Queue<GameObject>();
 
         GameObject root = Instantiate(branch, transform.position, 
-            new Quaternion(0, 0, 0, 0), transform);
+            new Quaternion(0, 0, 0, 0), branchesPool.transform);
         root.transform.localScale = new Vector3(thickness, height, thickness);
         branches.Enqueue(root);                
 
         while (branches.Count != 0)
         {
 
-            GameObject br = branches.Dequeue();
+            GameObject parentBranch = branches.Dequeue();
 
-            Vector3 branchScale = br.transform.localScale / 2;
+            Vector3 branchScale = parentBranch.transform.localScale / 2;
 
-            if (branchScale.y < leafSize / 2)
+            if (branchScale.y > leafSize)
             {
 
-                Instantiate(leaf,
-                    br.transform.localPosition + br.transform.up * branchScale.y,
-                    new Quaternion(0, 0, 0, 0),
-                    transform).transform.localScale = new Vector3(leafSize, leafSize, leafSize);
+                for (int i = 0; i < branchesNumber; ++i)
+                {
 
-                continue;
+                    GameObject branchClone = Instantiate(branch,
+                        parentBranch.transform.localPosition + parentBranch.transform.up * branchScale.y,
+                        parentBranch.transform.localRotation * Quaternion.Euler(-45, radiansBetweenBranches * i, 0),
+                        branchesPool.transform);
 
-            }
-            
-            for (int i = 0; i < branchesNumber; ++i)
-            {
+                    branchClone.transform.localScale = branchScale;
 
-                GameObject branchClone = Instantiate(branch,
-                    br.transform.localPosition + br.transform.up * branchScale.y,
-                    br.transform.localRotation * Quaternion.Euler(-45, radiansBetweenBranches * i, 0),
-                    transform);
+                    branches.Enqueue(branchClone);
 
-                branchClone.transform.localScale = branchScale;             
-
-                branches.Enqueue(branchClone);                              
+                }
 
             }
 
             Instantiate(leaf,
-                    br.transform.localPosition + br.transform.up * br.transform.localScale.y,
-                    new Quaternion(0, 0, 0, 0),
-                    transform).transform.localScale = new Vector3(br.transform.localScale.x * 2, br.transform.localScale.x * 2, br.transform.localScale.x * 2);
+                    parentBranch.transform.localPosition + parentBranch.transform.up * parentBranch.transform.localScale.y,
+                    parentBranch.transform.localRotation/*new Quaternion(0, 0, 0, 0)*/,
+                    leafsPool.transform).transform.localScale
+                    = new Vector3(parentBranch.transform.localScale.x * 2,
+                    parentBranch.transform.localScale.x * 2,
+                    parentBranch.transform.localScale.x * 2);
 
         }
+
+        CombineMesh(branchesPool);
+        CombineMesh(leafsPool);
+
+    }
+
+    private void CreatePool(out GameObject pool, GameObject type, string name)
+    {
+
+        pool = new GameObject(name, typeof(MeshFilter));
+
+        pool.transform.parent = transform;
+
+        MeshRenderer bpMeshRenderer = pool.AddComponent<MeshRenderer>();
+        bpMeshRenderer.material = type.GetComponentInChildren<MeshRenderer>().sharedMaterial;
+
+    }
+
+    private void CombineMesh(GameObject obj)
+    {
+
+        //Set position to zero for easier matrix math
+        Vector3 position = obj.transform.position;
+        obj.transform.position = Vector3.zero;
+
+        MeshFilter[] meshFilters = obj.GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length - 1];        
+
+        //From 1 cause 0 is parent
+        for (int i = 1; i < meshFilters.Length; ++i)
+        {
+
+            combine[i - 1].mesh = meshFilters[i].sharedMesh;
+            combine[i - 1].transform = meshFilters[i].transform.localToWorldMatrix;
+            Destroy(meshFilters[i].gameObject);
+
+        }
+
+        obj.transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        obj.transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine, true, true);
+        obj.transform.gameObject.SetActive(true);
+
+        //Return to initial position
+        obj.transform.position = position;
 
     }
 
